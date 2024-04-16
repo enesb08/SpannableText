fe2cb19b205bf266a59ca0e81250913bd1188fcf
enesb08 <enesbaskaya08@gmail.com>
2024-04-16 14:41:54 +0300

    rempved unused import

--- app/src/main/java/com/ebmobile/spannabletext/MainActivity.kt
+++ app/src/main/java/com/ebmobile/spannabletext/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
